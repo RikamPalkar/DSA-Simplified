40f12d7e40861c4155578ccc1456595a6d2f5917
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2022-08-23 14:20:21 +0530

    InsertionSort

---
+++ Sorting/InsertionSort.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{    
+    /*
+             Time-Complexity : Space-Complexity
+       Best :   O(n)         :     O(1)
+       Avg  :   O(n^2)       :     O(1)
+       Worst:   O(n^2)       :     O(1)
+    */
+    public static int[] InsertionSort(int[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            int j = i;
+            while (j > 0 && array[j] < array[j - 1])
+            {
+                int temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+                j--;
+            }
+        }
+        return array;
+    }
+}
