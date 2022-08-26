5e834559739f78341fb456de48b3268fea3528e4
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2022-08-26 23:35:07 +0530

    SelectionSort

---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static int[] SelectionSort(int[] array)
+    {
+        if (array.Length == 0)
+        {
+            return Array.Empty<int>();
+        }
+        if (array.Length == 1)
+        {
+            return array;
+        }
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int lowest = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[lowest])
+                {
+                    lowest = j;
+                }
+            }
+            (array[i], array[lowest]) = (array[lowest], array[i]);
+        }
+        return array;
+    }
+}
