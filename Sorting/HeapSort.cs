8cc94c094797c964787a9c9570b163b632ca97e0
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2022-08-26 23:05:44 +0530

    HeapSort

---
+++ Sorting/HeapSort.cs
@@ -0,0 +1,51 @@
+﻿public class Program
+{
+    public static int[] HeapSort(int[] array)
+    {
+        BuildMaxHeap(array);
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            Swap(0, i, array);
+            SiftDown(0, i - 1, array);
+        }
+        return array;
+    }
+
+    private static void BuildMaxHeap(int[] array)
+    {
+        int lastParent = (array.Length - 2) / 2;
+        for (int i = lastParent; i >= 0; i--)
+        {
+            SiftDown(i, array.Length - 1, array);
+        }
+    }
+
+    private static void SiftDown(int startIdx, int endIdx, int[] array)
+    {
+        int firstChild = startIdx * 2 + 1;
+        while (firstChild <= endIdx)
+        {
+            int secondChild = startIdx * 2 + 2 <= endIdx ? startIdx * 2 + 2 : -1;
+            int idxToSwap = firstChild;
+            if (secondChild != -1 && array[secondChild] > array[firstChild])
+            {
+                idxToSwap = secondChild;
+            }
+            if (array[idxToSwap] > array[startIdx])
+            {
+                Swap(idxToSwap, startIdx, array);
+                startIdx = idxToSwap;
+                firstChild = startIdx * 2 + 1;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    private static void Swap(int i, int j, int[] array)
+    {
+        (array[j], array[i]) = (array[i], array[j]);
+    }
+}
