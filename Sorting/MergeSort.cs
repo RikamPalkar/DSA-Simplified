4ab2b516f2bbe83c7f034595282203670984f04a
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2022-08-27 16:22:36 +0530

    MergeSort

---
+++ Sorting/MergeSort.cs
@@ -0,0 +1,58 @@
+﻿
+public class Program
+{
+    public static int[] MergeSort(int[] array)
+    {
+        //Half way when arrays are divided into 1,1
+        if (array.Length <= 1)
+        {
+            return array;
+        }
+
+        int mid = array.Length / 2;
+        int[] leftArray = array.Take(mid).ToArray();
+        int[] rightArray = array.Skip(mid).ToArray();
+
+        return SortSplittedArrays(MergeSort(leftArray), MergeSort(rightArray));
+    }
+
+    private static int[] SortSplittedArrays(int[] leftArray, int[] rightArray)
+    {
+        int[] mergedArray = new int[leftArray.Length + rightArray.Length];
+        int leftArrayPointer = 0;
+        int rightArrayPointer = 0;
+        int resultArrayPointer = 0;
+
+        while (leftArrayPointer < leftArray.Length && rightArrayPointer < rightArray.Length)
+        {
+            if (leftArray[leftArrayPointer] < rightArray[rightArrayPointer])
+            {
+                mergedArray[resultArrayPointer] = leftArray[leftArrayPointer];
+                leftArrayPointer++;
+                resultArrayPointer++;
+            }
+            else
+            {
+                mergedArray[resultArrayPointer] = rightArray[rightArrayPointer];
+                rightArrayPointer++;
+                resultArrayPointer++;
+            }
+        }
+        //Left outliers
+        while (leftArrayPointer < leftArray.Length)
+        {
+            mergedArray[resultArrayPointer] = leftArray[leftArrayPointer];
+            leftArrayPointer++;
+            resultArrayPointer++;
+        }
+
+        //Right outliers
+        while (rightArrayPointer < rightArray.Length)
+        {
+            mergedArray[resultArrayPointer] = rightArray[rightArrayPointer];
+            rightArrayPointer++;
+            resultArrayPointer++;
+        }
+        return mergedArray;
+    }
+}
