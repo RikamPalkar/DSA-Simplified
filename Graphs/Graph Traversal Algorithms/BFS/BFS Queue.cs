2518f8187199dae954dc1f84178bedb3f44e6c1b
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2022-09-17 17:56:45 +0530

    BFS

---
+++ Graphs/Graph Traversal Algorithms/BFS/BFS Queue.cs
@@ -0,0 +1,35 @@
+﻿
+public class Node
+{
+    public string name;
+    public List<Node> children = new List<Node>();
+
+    public Node(string name)
+    {
+        this.name = name;
+    }
+
+    public List<string> BreadthFirstSearch(List<string> array)
+    {
+        Queue<Node> bfs = new Queue<Node>();
+        bfs.Enqueue(this);
+        while (bfs.Count != 0)
+        {
+            Node currentNode = bfs.Dequeue();
+            array.Add(currentNode.name);
+            foreach (Node currentChild in currentNode.children)
+            {
+                bfs.Enqueue(currentChild);
+            }
+        }
+        return array;
+    }
+
+    public Node AddChild(string name)
+    {
+        Node child = new Node(name);
+        children.Add(child);
+        return this;
+    }
+}
+
---
+++ Graphs/Graph Traversal Algorithms/BFS/BFS Recursion.cs
@@ -0,0 +1,29 @@
+﻿
+public class Node
+{
+    public string name;
+    public List<Node> children = new List<Node>();
+
+    public Node(string name)
+    {
+        this.name = name;
+    }
+
+    public List<string> BreadthFirstSearch(List<string> array)
+    {
+        array.Add(this.name);
+        foreach (Node currentChild in children)
+        {
+            BreadthFirstSearch(array);
+        }
+        return array;
+    }
+
+    public Node AddChild(string name)
+    {
+        Node child = new Node(name);
+        children.Add(child);
+        return this;
+    }
+}
+
