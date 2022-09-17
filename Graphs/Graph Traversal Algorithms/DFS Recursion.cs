44ee49864bec6b7b4bbad70b84a9c9046e98a5a8
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2022-09-17 17:28:00 +0530

    DFS

---
+++ Graphs/Graph Traversal Algorithms/DFS Recursion.cs
@@ -0,0 +1,29 @@
+﻿
+public class Node
+{
+    public string name;
+    public List<Node> children = new List<Node>();
+
+    public Node(string name)
+    {
+        this.name = name;
+    }
+
+    //time: O(v + e), space: O(v)
+    public List<string> DepthFirstSearch(List<string> array)
+    {
+        array.Add(this.name);
+        foreach (Node node in children)
+        {
+            node.DepthFirstSearch(array);
+        }
+        return array;
+    }
+
+    public Node AddChild(string name)
+    {
+        Node child = new Node(name);
+        children.Add(child);
+        return this;
+    }
+}
---
+++ Graphs/Graph Traversal Algorithms/DFS Stack.cs
@@ -0,0 +1,35 @@
+﻿
+public class Node
+{
+    public string name;
+    public List<Node> children = new List<Node>();
+
+    public Node(string name)
+    {
+        this.name = name;
+    }
+
+    public List<string> DepthFirstSearch(List<string> array)
+    {
+        Stack<Node> dfsStack = new Stack<Node>();
+        dfsStack.Push(this);
+        while (dfsStack.Count != 0)
+        {
+            Node currentPoppedNode = dfsStack.Pop();
+            array.Add(currentPoppedNode.name);
+            for (int i = currentPoppedNode.children.Count - 1; i >= 0; i--)
+            {
+                dfsStack.Push(currentPoppedNode.children[i]);
+            }
+        }
+        return array;
+    }
+
+    public Node AddChild(string name)
+    {
+        Node child = new Node(name);
+        children.Add(child);
+        return this;
+    }
+}
+
