dd48e93b58831218c91b1058ea1c2c29727fe62d
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2024-11-04 13:21:37 +0530

    Update 3163. String Compression III.cs

--- LeetCode/3163. String Compression III.cs
+++ LeetCode/3163. String Compression III.cs
@@ -1,1 +1,21 @@
+//Time: O(n) | Space: O(n)
+    public string CompressedString(string word) 
+    {
+        StringBuilder sb = new();
 
+        for(int i=0; i<word.Length; i++)
+        {
+            int count = 1;
+            while(i < word.Length-1 && word[i] == word[i+1])
+            {         
+                if(count == 9) break;
+                count++;
+                i++;
+            }
+
+            sb.Append(count);
+            sb.Append(word[i]);
+        }
+        return sb.ToString();
+
+    }
