c4db9dfc42457e9cd725b6b8e34dda5b5c3695b5
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2025-06-08 18:34:35 +0530

    Update 386. Lexicographical Numbers.cs

--- LeetCode/386. Lexicographical Numbers.cs
+++ LeetCode/386. Lexicographical Numbers.cs
@@ -1,24 +1,24 @@
 public class Solution {
+
     public IList<int> LexicalOrder(int n) {
+        IList<int> res = [];
 
-       List<int> lexicalNumbers = [];
-       for(int i=1; i<=9; i++)
-       {
-            DFS(i, n, lexicalNumbers);
-       } 
-       return lexicalNumbers;
+        for(int i=1; i<=9; i++)
+            DFS(i, res, n);
+
+        return res;
     }
-    
-    private void DFS(int curr, int n, List<int> lexicalNumbers)
-    {
-        if(curr >  n) return;
-        lexicalNumbers.Add(curr);
 
-        int nextTenDigits = curr*10;
+    private void DFS(int index, IList<int> res, int n){
+        if(index > n) return;
 
-        for(int i=0; i<=9; i++)
-        {
-            DFS(i+nextTenDigits, n, lexicalNumbers);
-        } 
+        res.Add(index);
+        index = index*10;
+
+        if(index > n) return;//No point of going further
+
+        for(int i = index; i <= index+9; i++){
+            DFS(i, res, n);
+        }
     }
 }
